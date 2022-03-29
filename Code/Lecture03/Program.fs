1623197e68ca56ce3ac25c1f8fd3f8f38e533369
Vincenzo Ciancia <vincenzoml@gmail.com>
2022-03-29 13:08:55 +0100

    Lecture 3

--- Code/Lecture03/Program.fs
+++ Code/Lecture03/Program.fs
@@ -139,11 +139,167 @@ let functional3 = functional2 succ
 
 printfn "%A" (functional3 3)
 
+// Conditional expressions
+
+let val1 = 3
+let cond1 = if val1 > 4 then 1 else 0
+
+// Note: boolean expressions are also expressions:
+
+let bool1 = val1 > 4
+let cond2 = if bool1 then 1 else 0
+
+// Recursion!
+
+let rec recFn1 x = 
+    if x <= 0 then 1 else 1 + (recFn1 (x-1))
+
+// Try to remove rec see what's the error. Why?
+
+let rec recFn2 x = 
+    if x <= 0 then 0        
+    else 2 + (recFn2 (x - 1))
+
+// Aha! Let's see:
+// let rec recFn3 x = 
+//     x + (recFn3 x)
+
+/////// 
+/// Exercises: define the following functions, each time test them on a few cases.
+
+/// Fibonacci
+
+/// Sum of the first k numbers
+
+/// Product of the first k numbers
+
+/// Sum for i in [1,k] of f(i) 
+
+/// Sum for i in [1,k] of f(i), defined using a function, taking as arguments k and f
+
+/// Same for product
+
+/// Now take also the sum or product as an argument, and take an interval that is: 
+// define a function fold taking three arguments: a binary function g, a unary function f, two extremes k1 and k2, a "initial value" z (which is 0 for the sum and 1 for the product) 
+// "fold g f k1 k2 z" must compute g(g(z,f(k1)),f(k1+1),...), for instance "fold sum f 1 n 0" is the sum for i in [1,n] of f(i).
+
 // Lists
 
-// Arrays
+let list1 = [1; 2; 3]
 
-// Pattern Matching
+let list2 = [1,2,3]
+let list2' = [(1,2,3)]
+
+let list3 = (1,2,3)
+
+// Q: What is the difference?
+
+let list4 = [] // empty list
+
+printfn "%A %A %A" list1.[0] list1.[1] list1.[2]
+
+printfn "%A %A" (List.head list1) (List.tail list1)
+
+let rec length lst = 
+    if lst = [] 
+    then 0
+    else 1 + (length (List.tail lst))
+
+printfn "%A" (length list1)
+printfn "%A" (length list2)
+printfn "%A" (length list2')
+
+// "Cons" operator (see LISP)
+
+let list5 = 4::list1
+
+let list6 = 4::5::list1
+
+let list6' = 4::(5::list1) // it's the same
+
+let rec evenPositionsRec i lst =
+    if lst = [] then []
+    else 
+        if i % 2 = 0 
+        then (List.head lst)::(evenPositionsRec (i+1) (List.tail lst))
+        else evenPositionsRec (i+1) (List.tail lst)
+
+printfn "%A" (evenPositionsRec 0 [1;2;3;4;5])
+
+let evenPositions lst = evenPositionsRec 0 lst
+
+// ALSO:
+let evenPositions' lst =
+    let rec aux i lst =
+        if lst = [] then []
+        else 
+            if i % 2 = 0 
+            then (List.head lst)::(aux (i+1) (List.tail lst))
+            else aux (i+1) (List.tail lst)
+    aux 0 lst
+
+/// Failure?
+/// 
+
+let rec firstElement (lst : list<int>) =
+    if lst <> [] then List.head lst
+    else failwith (sprintf "Error: there are no elements in the list %A" lst)
+
+/// Note: fsharp like csharp and java has a sophisticated exception handling mechanism that we are just deliberately ignoring and super-simplifying
+
+/// Exercises
+
+/// Function to reverse a list
+/// 
+/// Function to sum all the elements of a list
+/// 
+/// Function to find the maximum of a list
+/// 
+/// Function to find the minimum of a list
+/// 
+/// Function to find both maximum and minimum in one pass
+/// 
+/// Function to "intersperse" a list with a value, e.g.
+/// intersperse "-" ["a";"b";"c"] = ["a";"-";"b";"-";"c"]
+/// 
+/// Note: after "c" there should NOT be an occurrence of "-"
+/// 
+/// Function to concatenate two lists
+/// concat [1;2] [3;4] = [1;2;3;4]
+/// 
+/// Function to multiply two lists value by value; raise an error if one is longer than the other one. 
+/// multiplyLists [1;2;3] [4;5;6] = [4;10;18]
+
+// Function to "zip" a list: from a pair of lists, obtain a list of pairs
+
+// Function to unzip a list: from a list of pairs, obtain two lists
+
+// Don't look at the solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let rec unzip l = 
+    if l = [] then ([],[])
+    else 
+        let hd = List.head l
+        let tl = unzip (List.tail l)
+        ((fst hd)::(fst tl),(snd hd)::(snd tl))
+
+// Pattern matching (start by two chained ifs)
+
+// Arrays
 
 // Records
 
